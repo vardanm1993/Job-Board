2fddf1bbcffda6cf902ebff6840aa9f3250e9412
vardanm1993 <vardanm1993@gmail.com>
2024-01-20 16:58:40 +0000

    Added index page with job card and link  component

---
+++ resources/views/job/index.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    @foreach($jobs as $job)
+      <x-job-card class="mb-4" :$job>
+          <div>
+              <x-link-button :href="route('jobs.show', $job)">
+                  Show
+              </x-link-button>
+          </div>
+      </x-job-card>
+    @endforeach
+</x-layout>
