ae4c40de6eaff7149c4f8ec29e339135a12d1bcc
vardanm1993 <vardanm1993@gmail.com>
2024-01-20 16:53:20 +0000

    Added job resource route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('',fn() => to_route('jobs.index'));
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('jobs', JobController::class);
