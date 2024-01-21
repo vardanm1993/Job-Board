e79a02b8c7203b2aa632b64192118c4060797c80
vardanm1993 <vardanm1993@gmail.com>
2024-01-21 15:17:50 +0000

    Added search by name and salary logic

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -12,7 +12,20 @@ class JobController extends Controller
      */
     public function index()
     {
-        return view('job.index', ['jobs' => Job::all()]);
+        $jobs = Job::query();
+
+        $jobs->when(request('search'), function ($query) {
+            $query->where(function ($query){
+                $query->where('title', 'like', '%' . request('search') . '%')
+                    ->orWhere('description', 'like', '%' . request('search') . '%');
+            });;
+        })->when(request('min_salary'), function ($query) {
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function ($query) {
+            $query->where('salary', '<=', request('max_salary'));
+        });;
+
+        return view('job.index', ['jobs' => $jobs->get()]);
     }
 
     /**
