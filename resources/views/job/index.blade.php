43a9404e453792b536c899e7937a8ebf053eea71
vardanm1993 <vardanm1993@gmail.com>
2024-01-21 15:16:20 +0000

    Added serach and salary view with text input components

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,6 +1,25 @@
 <x-layout>
     <x-breadcrumbs class="mb-4"
                    :links="['Jobs' => route('jobs.index')]"/>
+
+    <x-card class="mb-4 text-sm">
+        <form action="{{ route('jobs.index') }}" method="GET">
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                    <div class="mb-1 font-semibold">Search</div>
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">Salary</div>
+                    <div class="flex space-x-2">
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
+                    </div>
+                </div>
+            </div>
+            <button class="w-full">Filter</button>
+        </form>
+    </x-card>
     @foreach($jobs as $job)
         <x-job-card class="mb-4" :$job>
             <div>
