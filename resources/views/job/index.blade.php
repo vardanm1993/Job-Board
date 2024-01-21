b87a117fe12570b0947ba280cd31f46e57d78ff4
vardanm1993 <vardanm1993@gmail.com>
2024-01-21 12:28:33 +0000

    Added breadcrumbs component for index

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,11 +1,13 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4"
+                   :links="['Jobs' => route('jobs.index')]"/>
     @foreach($jobs as $job)
-      <x-job-card class="mb-4" :$job>
-          <div>
-              <x-link-button :href="route('jobs.show', $job)">
-                  Show
-              </x-link-button>
-          </div>
-      </x-job-card>
+        <x-job-card class="mb-4" :$job>
+            <div>
+                <x-link-button :href="route('jobs.show', $job)">
+                    Show
+                </x-link-button>
+            </div>
+        </x-job-card>
     @endforeach
 </x-layout>
