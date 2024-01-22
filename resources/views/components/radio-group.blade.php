34d478047f3b4f620ca738100e803b3917092ebe
vardanm1993 <vardanm1993@gmail.com>
2024-01-22 17:41:40 +0000

    Added labels

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -4,11 +4,11 @@
             @checked(!request($name))/>
         <span class="ml-2">All</span>
     </label>
-    @foreach($options as $option)
+    @foreach($optionsWithLabels as $label => $option)
         <label for="{{ $name }}" class="mb-1 flex items-center">
             <input type="radio" name="{{ $name }}" value="{{ $option }}"
                 @checked($option === request($name)) />
-            <span class="ml-2">{{ $option }}</span>
+            <span class="ml-2">{{ $label }}</span>
         </label>
     @endforeach
 </div>
