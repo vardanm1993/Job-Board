aaff42cd2b688f26c253273e755f142a053beba4
vardanm1993 <vardanm1993@gmail.com>
2024-01-22 19:10:04 +0000

    Updated font awesome icon  to heroicons icon

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,11 +1,13 @@
 <nav {{ $attributes }}>
-    <ul class="flex space-x-2 text-slate-500">
+    <ul class="flex space-x-2 text-slate-500 h-full items-center">
         <li>
             <a href="/">Home</a>
         </li>
         @foreach($links as $label => $link)
             <li>
-                <i class="fa-solid fa-arrow-right"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <path fill-rule="evenodd" d="M3 10a.75.75 0 0 1 .75-.75h10.638L10.23 5.29a.75.75 0 1 1 1.04-1.08l5.5 5.25a.75.75 0 0 1 0 1.08l-5.5 5.25a.75.75 0 1 1-1.04-1.08l4.158-3.96H3.75A.75.75 0 0 1 3 10Z" clip-rule="evenodd" />
+                </svg>
             </li>
             <li>
                 <a href="{{ $link }}">
