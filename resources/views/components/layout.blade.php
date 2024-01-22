22442494fb7c38cb8d7886aa7cdb3138557c3be8
vardanm1993 <vardanm1993@gmail.com>
2024-01-22 19:10:32 +0000

    Added app js with vite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
 
     <title>Job Board</title>
 
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 
 </head>
 <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
