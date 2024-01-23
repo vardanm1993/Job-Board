8b0a2296f78788090916d9bd0cbebe2b124d54f5
vardanm1993 <vardanm1993@gmail.com>
2024-01-23 16:57:57 +0000

    Added AlpineJs with start

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,6 +5,8 @@
  */
 
 import axios from 'axios';
+import Alpine from 'alpinejs';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -30,3 +32,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+
+window.Alpine = Alpine;
+Alpine.start();
