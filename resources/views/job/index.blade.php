e445790ae4e95754fb8dd5887fcd8359db7212e0
vardanm1993 <vardanm1993@gmail.com>
2024-01-23 17:06:52 +0000

    Added  refs for alpine and button component

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,18 +2,18 @@
     <x-breadcrumbs class="mb-4"
                    :links="['Jobs' => route('jobs.index')]"/>
 
-    <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET" id="filtering-form">
+    <x-card class="mb-4 text-sm" x-data="">
+        <form x-ref="filters" action="{{ route('jobs.index') }}" method="GET" id="filtering-form">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" form-id="filtering-input"/>
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" form-ref="filtering-input"/>
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" form-id="filtering-input"/>
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" form-id="filtering-input"/>
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" form-ref="filtering-input"/>
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" form-ref="filtering-input"/>
                     </div>
                 </div>
                 <div>
@@ -29,7 +29,7 @@
                                    :options="\App\Models\Job::$category"/>
                 </div>
             </div>
-            <button class="w-full">Filter</button>
+            <x-button class="w-full">Filter</x-button>
         </form>
     </x-card>
     @foreach($jobs as $job)
