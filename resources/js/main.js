6f0a1979b1d5a6fed6d34bf69888e43bac47eb39
vardanm1993 <vardanm1993@gmail.com>
2024-01-23 16:54:05 +0000

    Added AlpineJs

--- resources/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function clearInput(name){
-    document.getElementById(name).value = '';
-    document.getElementById('filtering-form').submit()
-}
-
-window.clearInput = clearInput;
-
