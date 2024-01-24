0af5abded586e262a2e2628c2b1730fe345fc351
vardanm1993 <vardanm1993@gmail.com>
2024-01-24 20:25:30 +0000

    Added user,employer and jobs for seeding  with ralationships

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Employer;
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,12 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Job::factory(100)->create();
-        // \App\Models\User::factory(10)->create();
+        User::factory(200)->create();
+        $users = User::all()->shuffle();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 0; $i < 20; $i++){
+            Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+        $employers = Employer::all();
+
+        for ($i = 0; $i < 100; $i++){
+            Job::factory()->create([
+                'employer_id' => $employers->random()->id
+            ]);
+        }
+
     }
 }
