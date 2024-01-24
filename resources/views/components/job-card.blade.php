3afc80270f2eacae1ac230da6966bd7b0bfc6a03
vardanm1993 <vardanm1993@gmail.com>
2024-01-24 20:26:30 +0000

    Added company name

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -8,7 +8,7 @@
 
     <div class="flex justify-between mb-4 text-sm text-slate-500 items-center">
         <div class="flex space-x-4">
-            <div>Company Name</div>
+            <div>{{ $job->employer->company_name }}</div>
             <div>{{ $job->location }}</div>
         </div>
 
