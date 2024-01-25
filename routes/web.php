f3c29fd25117fd63767b0eb39ebeafd76ec6202a
vardanm1993 <vardanm1993@gmail.com>
2024-01-25 17:47:49 +0000

    Added auth resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('',fn() => to_route('jobs.index'));
+Route::get('', fn() => to_route('jobs.index'));
 
-Route::resource('jobs', JobController::class);
+Route::resource('jobs', JobController::class)
+    ->only(['index', 'show']);
+
+Route::get('login', fn() => to_route('auth.create'))
+    ->name('login');
+Route::resource('auth', AuthController::class)
+    ->only(['create', 'store']);;
+Route::delete('logout', fn() => to_route('auth.destroy'))
+    ->name('logout');
+Route::delete('auth', [AuthController::class, 'destroy'])
+    ->name('auth.destroy');
