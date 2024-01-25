d47883cd88fc90bd2667089981bc4500f59d8dce
vardanm1993 <vardanm1993@gmail.com>
2024-01-25 17:52:00 +0000

    Added nav for login and logout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,32 @@
 
 </head>
 <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-slate-700">
+    <nav class="mb-8 flex justify-between text-lg font-medium ">
+        <ul class="flex space-x-2">
+            <li>
+                <a href="{{ route('jobs.index') }}">Home</a>
+            </li>
+        </ul>
+        <ul class="flex space-x-2">
+            @auth
+                <li>
+                    {{ auth()->user()->name ?? 'Anonymous'}}
+                </li>
+                <li>
+                    <form action="{{ route('auth.destroy') }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        <button>Logout</button>
+                    </form>
+                </li>
+            @else
+                <li>
+                    <a href="{{ route('auth.create') }}">Sign in</a>
+                </li>
+            @endauth
+        </ul>
+    </nav>
     {{ $slot }}
 </body>
 </html>
