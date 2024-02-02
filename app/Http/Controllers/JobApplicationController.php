66748369d248402b2984503e512e34871077f643
vardanm1993 <vardanm1993@gmail.com>
2024-02-02 15:42:10 +0000

    Added custom job  policy for create

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -12,6 +12,7 @@ class JobApplicationController extends Controller
      */
     public function create(Job $job)
     {
+        $this->authorize('apply', $job);
         return view('job_application.create', compact('job'));
     }
 
