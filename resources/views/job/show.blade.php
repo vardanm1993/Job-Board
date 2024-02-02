39f253177985d7d9a0499937fce23a150ce4dfab
vardanm1993 <vardanm1993@gmail.com>
2024-02-02 15:43:17 +0000

    Added button apply with checking apply policy

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -5,6 +5,16 @@
         <p class="text-sm text-slate-500 mb-4">
             {!! nl2br(e($job->description)) !!}
         </p>
+
+        @can('apply', $job)
+            <x-link-button :href="route('job.application.create', $job)">
+                Apply
+            </x-link-button>
+        @else
+            <div class="text-center text-sm font-medium text-slate-500">
+                You have already applied to this job
+            </div>
+        @endcan
     </x-job-card>
     <x-card class="mb-4">
         <h2 class="mb-4 text-lg font-medium">
