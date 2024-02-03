23571afe9bf6a7ea54cbcc010b573953c4366b5d
vardanm1993 <vardanm1993@gmail.com>
2024-02-03 17:04:19 +0000

    Added logic for applied jobs with count, avg and logic for canceling

---
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobApplication;
+use Illuminate\Http\Request;
+
+class MyJobApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('my_job_application.index',
+            [
+                'applications' => auth()->user()->jobApplications()
+                    ->with([
+                        'job' => fn($query) => $query->withCount('jobApplications')
+                            ->withAvg('jobApplications', 'expected_salary'),
+                        'job.employer'
+                    ])
+                    ->latest()->get()
+            ]
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobApplication $myJobApplication)
+    {
+        $myJobApplication->delete();
+
+        return redirect()->back()->with(
+           'success',
+           'Job applications removed.'
+        );
+    }
+}
