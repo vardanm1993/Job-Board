3869767eafc3131ea6310da645d04b491f18e4ed
vardanm1993 <vardanm1993@gmail.com>
2024-02-03 16:59:31 +0000

    Added link to user applications page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,7 +19,9 @@
         <ul class="flex space-x-2">
             @auth
                 <li>
-                    {{ auth()->user()->name ?? 'Anonymous'}}
+                    <a href="{{ route('my-job-applications.index') }}">
+                        {{ auth()->user()->name ?? 'Anonymous'}}: Applications
+                    </a>
                 </li>
                 <li>
                     <form action="{{ route('auth.destroy') }}" method="POST">
