5df8d151a4edce91514b24e8f10a628d4d24ccd0
vardanm1993 <vardanm1993@gmail.com>
2024-02-05 14:06:02 +0000

    Added session error display

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -46,6 +46,13 @@
             <p>{{ session('success') }}</p>
         </div>
     @endif
+    @if(session('error'))
+        <div role="alert"
+            class="my-8 rounded-md border-l-4 border-red-400 bg-red-200 p-4 text-red-700 opacity-75">
+            <p class="font-bold">Error!</p>
+            <p>{{ session('error') }}</p>
+        </div>
+    @endif
 
     {{ $slot }}
 </body>
